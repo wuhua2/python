7b311ab28eadc2c47cb24d1f47495ebac63dfcdf
wuhua1 <1989823643@qq.com>
2020-01-27 12:06:50 +0800

    学习文件操作

---
+++ file.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 11:24:02 2020
+
+@author: root
+"""
+
+with open("plt.py") as f:
+    for line in f.readlines():
+        print(line)
+with open("f:/1.txt","a") as f:
+    f.write("我是神仙")
